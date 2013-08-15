dc5df1a18b185b7c618a8c29f9fc7528f53c546b
Ubuntu <ubuntu@ip-172-31-28-38.us-west-2.compute.internal>
2013-08-15 15:24:30 +0000

    m

--- web.js
+++ web.js
@@ -18,6 +18,11 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
+app.get('/contact.html', function(request, response) {
+  var data = fs.readFileSync('contact.html').toString();
+  response.send(data);
+});
+
 // Render example.com/orders
 app.get('/orders', function(request, response) {
   global.db.Order.findAll().success(function(orders) {
