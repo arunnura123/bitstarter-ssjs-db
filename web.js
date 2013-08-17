a692e7d0403eaf5cbbcbc523a11e4007e2f96fa4
Ubuntu <ubuntu@ip-172-31-28-38.us-west-2.compute.internal>
2013-08-17 17:03:04 +0000

    m

--- web.js
+++ web.js
@@ -29,7 +29,8 @@ app.get('/', function(request, response) {
 
 app.post('/', function (request, response) {
 var data = fs.readFileSync('contact.html').toString();
-response.send(data + "THANKS FOR YOUR SUBMISSION. WE WILL GET BACK TO YOU ON " + request.body.mail);
+response.render('/contact.html');
+response.send(data + " < PERFECT !!! WE WILL GET BACK TO YOU ON " + request.body.mail + ">");
 });
 
 
