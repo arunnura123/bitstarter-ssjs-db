ce147c810a09ec7d1a5cc96ac6f0f5894bc49d94
Ubuntu <ubuntu@ip-172-31-28-38.us-west-2.compute.internal>
2013-08-17 16:59:49 +0000

    m

--- web.js
+++ web.js
@@ -28,8 +28,8 @@ app.get('/', function(request, response) {
 
 
 app.post('/', function (request, response) {
-var data = fs.readFileSync('index.html').toString();
-response.send("THANKS FOR YOUR SUBMISSION. WE WILL GET BACK TO YOU ON " + request.body.mail + "PLEASE REFRESH AND SHARE" + data );
+var data = fs.readFileSync('contact.html').toString();
+response.send(data + "THANKS FOR YOUR SUBMISSION. WE WILL GET BACK TO YOU ON " + request.body.mail);
 });
 
 
