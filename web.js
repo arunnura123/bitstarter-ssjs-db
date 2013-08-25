0efcc4131b79c80154707637e18f7886d04a4e42
Ubuntu <ubuntu@ip-172-31-28-38.us-west-2.compute.internal>
2013-08-25 17:17:24 +0000

    m

--- web.js
+++ web.js
@@ -23,9 +23,9 @@ app.use(app.router);
 
 app.post('/', function (request, response) {
  var data = fs.readFileSync('index.html').toString();
- var wStrin = request.body.what.replace(/ /g, "_");
- var hStrin =  request.body.where.replace(/ /g, "_");
- var mStrin =  request.body.mail.replace(/ /g, "_"); 
+ var wStrin =  "'"+request.body.what.replace(/ /g, "_")+"'";
+ var hStrin =  "'"+request.body.where.replace(/ /g, "_")+"'";
+ var mStrin =  "'"+ request.body.mail.replace(/ /g, "_")+"'"; 
  pg.connect(conf, function(err, client, done) {
  if(err) return console.error(err);
   client.query("INSERT INTO whatuneed(need,location,mail) VALUES(wStrin,hStrin,mStrin)", function(err, result) {
