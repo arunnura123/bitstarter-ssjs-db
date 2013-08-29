7eafe55f025f473e42890b766d4cf1f5d8be0e9e
Ubuntu <ubuntu@ip-172-31-28-38.us-west-2.compute.internal>
2013-08-29 08:49:42 +0000

    m

--- web.js
+++ web.js
@@ -112,7 +112,7 @@ app.get('/Visuals.html', function(request, response) {
   client.query('SELECT * FROM whatuneed', function(err, result) {
     done();
     if(err) return console.error(err);
-  response.send (result.rows.length);
+  response.send('result.rows.length');
   });
 });
  
