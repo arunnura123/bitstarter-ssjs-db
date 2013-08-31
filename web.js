3c6ab5e808892b130602a558efabd0b589b808f8
Ubuntu <ubuntu@ip-172-31-28-38.us-west-2.compute.internal>
2013-08-31 06:59:59 +0000

    m

--- web.js
+++ web.js
@@ -129,8 +129,9 @@ if(!wStrin || !hStrin || !mStrin)
 
 
 app.get('/Info.html', function (request, response) {
-var data = fs.readFileSync('Infof.html').toString();
+var mdata = fs.readFileSync('Infof.html').toString();
 var bdata = fs.readFileSync('infob.html').toString();
+var data='';
 pg.connect(conf, function(err, client, done) {
  if(err) return console.error(err);
   client.query('SELECT need,location,ip,place FROM whatuneed', function(err, result) {
